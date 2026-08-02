c950807229b1c72107d02b7784705fc4fbcc7c95
Kunalgadgi <gadgikunal202@gmail.com>
2026-08-02 17:57:06 +0530

    Fix ALLOWED_HOSTS for Vercel

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -29,8 +29,12 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
-
+# ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+    "kunal-erp-portfolio.vercel.app",
+]
 
 # Application definition
 
